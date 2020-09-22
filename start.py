9e019ad64831ad0a1905dc6201d6f519eff56e0d
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-22 12:20:15 +0200

    Python alapok

---
+++ start.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 12:04:37 2020
+
+@author: SysAdmin
+"""
+# Szöveg kiírás
+print("Hello World!")
+
+print('Hello World')
+
+# Számok esetén az eredmény
+#jelenik meg
+print(2+8+21)
+
+# Ha kell, zárójelezünk
+print((2*10)+251)
+
+# tört
+print(10/3)
+
+# 0-val való osztás hiba
+# print(10/0)
+
+# A számokat most karakterként
+# értelmezi a program, összefűzi őket
+print('2'+'2')
+
+# háromszor kiírja a szöveget
+print("valami "*3)
+
+#hatványozás
+print(2**2)
+
+
+
