f773457057ad2695f4456f89d50267b6ca61a9ad
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-24 12:00:18 +0200

    Update start2.py

--- start2.py
+++ start2.py
@@ -5,27 +5,37 @@ Created on Thu Sep 24 07:56:04 2020
 @author: SysAdmin
 """
 
-print("2"+"2")
-print("valami "+"szöveg")
-
-a=12
-b=25
-
-print(a+b)
-
-a="12"
-b="25"
-
-print(a+b)
-
-a="valami"
-b=3
-
-print(a*b)
-
+# változók
 a=10
 b=2
 
+print(a+b)
+
+a="10"
+b="2"
+
+print(a+b)
+
+
 # értékadó utasítások
+a=10
+b=2
+
+#a=a+b
+#a=a*b
+
+a+=b
+a*=b
+a-=b
+a/=b
+
+print(a)
+
+#bevitel a felhasználótól
+a=float(input())
+b=float(input())
+
+
+print(a+b)
 
 
