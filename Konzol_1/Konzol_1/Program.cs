4d02bc922e6be1a2f75ab234bb5131ad2265042b
szajberpiraty <szajberpiraty@gmail.com>
2021-01-26 09:07:08 +0100

    C# kezdés

---
+++ Konzol_1/Konzol_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzol_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam1;
+            szam1 = 112;
+
+            int szam2 = 233;
+
+            int szam3 = szam1 + szam2;
+
+            szam1 = 2044;
+                       
+
+            Console.WriteLine("C# kezdés");
+
+            //Variációk kiíratásra- egy soros komment
+            Console.WriteLine($"Szám1:{szam1},szám2:{szam2},szám3:{szam3}");
+            Console.WriteLine("{1},{0}",szam1,szam2);
+
+            /* Több
+              soros
+              komment */
+
+            Console.WriteLine($"Előjeles egész:{Int32.MinValue},{Int32.MaxValue}");
+
+            Console.ReadKey();
+        }
+    }
+}
