9533c5c8e61ddf2207633672e87761c231eac772
szajberpiraty <szajberpiraty@gmail.com>
2021-01-26 10:40:42 +0100

    Bekérés

---
+++ Konzol_1/Bekeres/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bekeres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg egy számot:");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Szám1:{szam1}");
+
+            Console.ReadKey();
+        }
+    }
+}
