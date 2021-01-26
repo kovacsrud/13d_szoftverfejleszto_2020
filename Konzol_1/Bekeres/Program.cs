57500493be580435bea545474d1db7503628a077
szajberpiraty <szajberpiraty@gmail.com>
2021-01-26 10:50:04 +0100

    Bekérés és összeadás

--- Konzol_1/Bekeres/Program.cs
+++ Konzol_1/Bekeres/Program.cs
@@ -13,7 +13,16 @@ namespace Bekeres
             Console.Write("Adj meg egy számot:");
             int szam1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Szám1:{szam1}");
+            Console.Write("Adj meg egy másik számot:");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            //A var használata esetén a fordító mindig a megfelelő
+            //típust fogja a változóhoz rendelni
+            var szam3 = szam1 + szam2;
+            
+            Console.WriteLine($"Szám1:{szam1},Szám2:{szam2},Összeg:{szam3}");
+
+
 
             Console.ReadKey();
         }
