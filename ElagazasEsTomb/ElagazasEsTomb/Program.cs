eb97b40cd4c2430fe35440f0ffcbc72ec42bc59e
szajberpiraty <szajberpiraty@gmail.com>
2021-01-28 08:24:28 +0100

    Elágazás létrehozása

---
+++ ElagazasEsTomb/ElagazasEsTomb/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElagazasEsTomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg egy számot:");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            if (szam>0)
+            {//kódblokk, az itt létrehozott változók lokális
+             //változók, és csak ebben a kódblokkban érhetők el   
+                Console.WriteLine("Pozitív");
+            } else if (szam<0)
+            {
+                Console.WriteLine("Negatív");
+            } else
+            {
+                Console.WriteLine("Nulla");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
