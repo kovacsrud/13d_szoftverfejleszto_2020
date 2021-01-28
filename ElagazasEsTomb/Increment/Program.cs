6ae3c50aa4131be2270654ea400b94a1c5113246
szajberpiraty <szajberpiraty@gmail.com>
2021-01-28 12:09:46 +0100

    increment

---
+++ ElagazasEsTomb/Increment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            //inkrementáló utasítás 1-el növeli az értéket
+            a++;
+
+            //inkrementáló utasítás 1-el növeli az értéket
+            ++a;
+            //Console.WriteLine(a);
+
+            int b = a++;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
