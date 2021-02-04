2983f404e7086a76ec12d1ca58833be3d3dcf3e7
szajberpiraty <szajberpiraty@gmail.com>
2021-02-04 10:02:15 +0100

    Listából választás

---
+++ Listak/Sorsolas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorsolas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            List<int> szamgomb = new List<int>();
+            List<int> kivalasztott = new List<int>();
+            int darab = 5;
+            int feltolt = 90;
+
+            for (int i = 0; i < feltolt; i++)
+            {
+                szamgomb.Add(i + 1);
+            }
+
+            //foreach (var i in szamgomb)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            for (int i = 0; i < darab; i++)
+            {
+                Console.WriteLine($"Számgomb elemszám:{szamgomb.Count}");
+                var elemindex = rand.Next(0, szamgomb.Count);
+                kivalasztott.Add(szamgomb[elemindex]);
+                szamgomb.RemoveAt(elemindex);
+                
+            }
+
+            kivalasztott.Sort();
+
+            foreach (var i in kivalasztott)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            foreach (var i in szamgomb)
+            {
+                Console.Write($"{i} ");
+            }
+            //Tegyük vissza a kivett elemeket a szamgomb listába!
+
+            Console.ReadKey();
+        }
+    }
+}
