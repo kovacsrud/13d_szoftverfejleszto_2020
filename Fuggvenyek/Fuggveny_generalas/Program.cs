06214e882cf519db037fd51b046811298689e92f
szajberpiraty <szajberpiraty@gmail.com>
2021-02-08 10:48:19 +0100

    Update Program.cs

--- Fuggvenyek/Fuggveny_generalas/Program.cs
+++ Fuggvenyek/Fuggveny_generalas/Program.cs
@@ -8,20 +8,29 @@ namespace Fuggveny_generalas
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int[] t1 = { 12, 34, 56, 778, 999 };
             int[] t2 = { 34, 556, 665, 123, 554, 22223, 778, 53 };
-            int[] t3 = { 121, 344, 565, 78, 199,561 };
-            int[] t4 = { 869, 445, 129, 376, 886,912,1024 };
+            int[] t3 = { 121, 344, 565, 78, 199, 561 };
+            int[] t4 = { 869, 445, 129, 376, 886, 912, 1024 };
 
-            for (int i = 0; i < t1.Length; i++)
-            {
-                Console.Write(t1[i]+" ");
-            }
-            Console.WriteLine();
+            TombList(t1);
+            TombList(t2);
+            TombList(t3);
+            TombList(t4);
 
             Console.ReadKey();
+        }
+
+        private static void TombList(int[] tomb)
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write(tomb[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
