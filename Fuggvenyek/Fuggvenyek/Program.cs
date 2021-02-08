443d47aeb05e5ba7a8dc5f5a8fef6c9276be8edc
szajberpiraty <szajberpiraty@gmail.com>
2021-02-08 09:07:37 +0100

    Függvények

---
+++ Fuggvenyek/Fuggvenyek/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuggvenyek
+{
+    class Program
+    {
+        //Overloading, függvény túlterhelés
+        /// <summary>
+        /// A legegyszerűbb kiíró függvény
+        /// </summary>
+        static void Kiir()
+        {
+            Console.WriteLine("Függvény, első verzió");
+        }
+
+        /// <summary>
+        /// Paraméterben várja a kiírandó szöveget
+        /// </summary>
+        /// <param name="szoveg"></param>
+        static void Kiir(string szoveg)
+        {
+            Console.WriteLine(szoveg);
+        }
+        /// <summary>
+        /// Két paraméterben lehet megadni a kiírandó szöveget
+        /// </summary>
+        /// <param name="sz1"></param>
+        /// <param name="sz2"></param>
+        static void Kiir(string sz1,string sz2)
+        {
+            Console.WriteLine($"{sz1},{sz2}");
+        }
+
+        static void Main(string[] args)
+        {
+            Kiir();
+            Kiir("Csodás hétfő reggel");
+            Kiir("C#", "programozás");
+            
+          
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
