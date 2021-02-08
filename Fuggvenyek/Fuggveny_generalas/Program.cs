29f689accf4c9297c38619235df2811f96fdf46a
szajberpiraty <szajberpiraty@gmail.com>
2021-02-08 10:02:33 +0100

    Tömb listázás

---
+++ Fuggvenyek/Fuggveny_generalas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuggveny_generalas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t1 = { 12, 34, 56, 778, 999 };
+            int[] t2 = { 34, 556, 665, 123, 554, 22223, 778, 53 };
+            int[] t3 = { 121, 344, 565, 78, 199,561 };
+            int[] t4 = { 869, 445, 129, 376, 886,912,1024 };
+
+            for (int i = 0; i < t1.Length; i++)
+            {
+                Console.Write(t1[i]+" ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
--- Fuggvenyek/Fuggvenyek/Program.cs
+++ Fuggvenyek/Fuggvenyek/Program.cs
@@ -30,18 +30,57 @@ namespace Fuggvenyek
         /// </summary>
         /// <param name="sz1"></param>
         /// <param name="sz2"></param>
-        static void Kiir(string sz1,string sz2)
+        static void Kiir(string sz1, string sz2)
         {
             Console.WriteLine($"{sz1},{sz2}");
         }
+
+        
+        static double Osszead(double a,double b=10)
+        {
+            return a + b;
+        }
+
+        static int Szoroz(int a)
+        {
+            a = a * a;
+            return a;
+        }
+        /// <summary>
+        /// Cím szerinti paraméterátadás
+        /// </summary>
+        /// <param name="a"></param>
+        static void Szoroz(ref int a)
+        {
+            a = a * a;
+        }
+
+
+        
 
         static void Main(string[] args)
         {
             Kiir();
             Kiir("Csodás hétfő reggel");
             Kiir("C#", "programozás");
-            
-          
+            var osszeg = Osszead(10, 30);
+            var alaposszeg = Osszead(40);
+            var dbosszeg = Osszead(12.567, 45.7789);
+            Console.WriteLine(osszeg);
+            Console.WriteLine(alaposszeg);
+            Console.WriteLine(dbosszeg);
+            Console.WriteLine(Osszead(100,10));
+            int a = 3;
+            //érték szerinti paraméterátadás
+            Console.WriteLine(Szoroz(a));
+            Console.WriteLine(a);
+            //cím szerinti paraméterátadás
+            Szoroz(ref a);
+            Console.WriteLine(a);
+
+
+
+
 
 
             Console.ReadKey();
