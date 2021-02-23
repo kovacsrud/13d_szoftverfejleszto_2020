8b343dfa4296fe171bfee1c4f3e7bac0619ff87a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2021-02-23 11:59:33 +0100

    Ciklus alkalmazása bekérés esetén

---
+++ Ciklus_gyakorlas/Ciklus_gyakorlas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciklus_gyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kérjünk be 2 számot, és osszuk el őket egymással!
+            Console.Write("A:");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B:");
+            var b = Convert.ToInt32(Console.ReadLine());
+            //Oldjuk meg, hogy a b változó értéke ne lehessen 0!
+
+            while(b==0)
+            {
+                Console.Write("B nem lehet 0! Újra add meg:");
+                try
+                {
+                    b = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);                    
+                }
+                
+            }
+
+            //típuskényszerítést használunk
+            var c = (double)a / b;
+
+            Console.WriteLine($"Az osztás eredménye:{c}");
+
+            Console.ReadKey();
+        }
+    }
+}
