81b652cf31afb73c1e45852e177865528295cea3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2021-02-23 08:08:38 +0100

    Update Program.cs

--- KeruletTerulet/KeruletTerulet/Program.cs
+++ KeruletTerulet/KeruletTerulet/Program.cs
@@ -8,8 +8,28 @@ namespace KeruletTerulet
 {
     class Program
     {
+
+        static double TeglalapKerulet(double a,double b)
+        {
+            return (a + b) * 2;
+        }
+
+        static double TeglalapTerulet(double a,double b)
+        {
+            return a * b;
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Add meg a téglalap A oldalát!:");
+            double aoldal = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Add meg a téglalap B oldalát!:");
+            var boldal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"A téglalap kerülete:{TeglalapKerulet(aoldal,boldal)}");
+
+            Console.WriteLine($"A téglalap területe:{TeglalapTerulet(aoldal,boldal)}");
 
 
 
