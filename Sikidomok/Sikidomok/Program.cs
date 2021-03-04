c91e5288059a571b188884e9270c46d68cdea0ae
szajberpiraty <szajberpiraty@gmail.com>
2021-03-04 10:08:02 +0100

    Síkidom példa

---
+++ Sikidomok/Sikidomok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sikidomok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            List<Teglalap> teglalapok = new List<Teglalap>();
+            var elemszam = rand.Next(50, 100 + 1);
+
+            for (int i = 0; i < elemszam; i++)
+            {
+                teglalapok.Add(new Teglalap(rand.Next(1, 20), rand.Next(1, 30)));
+            }
+
+            Console.WriteLine($"A lista elemszáma:{teglalapok.Count}");
+
+            foreach (var i in teglalapok)
+            {
+                Console.WriteLine($"A:{i.Aoldal},B:{i.Boldal},Kerület:{i.Kerulet()},Terület:{i.Terulet()}");
+            }
+
+            //Mennyi a kerületek átlaga?
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sikidomok/Sikidomok/Teglalap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sikidomok
+{
+    public class Teglalap
+    {
+        
+
+        public int Aoldal { get; set; }
+        public int Boldal { get; set; }
+
+        public Teglalap(int aoldal, int boldal)
+        {
+            Aoldal = aoldal;
+            Boldal = boldal;
+        }
+
+        public int Kerulet()
+        {
+            return (Aoldal + Boldal) * 2;
+        }
+
+        public int Terulet()
+        {
+            return Aoldal * Boldal;
+        }
+    }
+}
