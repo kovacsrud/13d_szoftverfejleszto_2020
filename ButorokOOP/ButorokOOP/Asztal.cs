4672ac21caa738799c668656c980ef0f95a703fa
szajberpiraty <szajberpiraty@gmail.com>
2021-03-16 10:52:59 +0100

    Osztályok létrehozása

---
+++ ButorokOOP/ButorokOOP/Asztal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButorokOOP
+{
+    public enum Alak
+    {
+        négyzet,
+        kör,
+        ellipszis,
+        téglalap,
+        hatszög
+    }
+    public class Asztal:Butor
+    {
+        public Alak AsztallapAlak { get; set; }
+        public bool Fiokos { get; set; }
+    }
+}
---
+++ ButorokOOP/ButorokOOP/Butor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButorokOOP
+{
+    public enum Anyag
+    {
+        fa,
+        fém,
+        műanyag
+    }
+    public class Butor
+    {
+        public int Suly { get; set; }
+        public int MaxMagassag { get; set; }
+        public Anyag JellemzoAnyag { get; set; }
+
+    }
+}
