8b71376ce847a56d81b2123763b393e5c040e174
szajberpiraty <szajberpiraty@gmail.com>
2021-03-16 12:08:26 +0100

    Update Program.cs

--- Kivetelek/Kivetelek/Program.cs
+++ Kivetelek/Kivetelek/Program.cs
@@ -13,17 +13,29 @@ namespace Kivetelek
             try
             {
                 int a = 120;
-                int b = 0;
+                int b = 12;
                 int c = a / b;
 
                 Console.WriteLine("A művelet eredménye:");
                 Console.WriteLine(c);
+                throw new ArithmeticException("Aritmetikai hiba!");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("B értéke nem lehet 0! Adjon meg mást!");
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.StackTrace);                
+                Console.WriteLine(ex.StackTrace);
             }
-
+            finally
+            {
+                Console.WriteLine("Ennek a blokknak az utasításai mindenféleképp végrehajtódnak");
+            }
 
             Console.WriteLine("A try-catch utáni utasítás");
             
