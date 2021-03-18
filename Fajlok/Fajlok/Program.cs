5426cf0bfe9ee0cda9a90a32a4f1db61df225706
szajberpiraty <szajberpiraty@gmail.com>
2021-03-18 09:17:48 +0100

    Fájlok betöltése

---
+++ Fajlok/Fajlok/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fajlok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fajl = new FileStream("snooker.txt",FileMode.Open);
+            StreamReader sr = new StreamReader(fajl,Encoding.Default);
+
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            sr.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
