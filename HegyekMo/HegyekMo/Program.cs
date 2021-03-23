ef1d9afe79d6cf11c5b69fc9e5a27617f6dac056
szajberpiraty <szajberpiraty@gmail.com>
2021-03-23 11:04:32 +0100

    Update Program.cs

--- HegyekMo/HegyekMo/Program.cs
+++ HegyekMo/HegyekMo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace HegyekMo
     {
         static void Main(string[] args)
         {
+            List<Hegycsucs> hegycsucsok = new List<Hegycsucs>();
+            try
+            {
+                var sorok = File.ReadAllLines("hegyekMo.txt",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    hegycsucsok.Add(new Hegycsucs(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"A hegyek száma:{hegycsucsok.Count}");
+
+            var atlagMagassag = hegycsucsok.Average(x=>x.Magassag);
+            Console.WriteLine($"A hegyek átlagos magassága:{atlagMagassag} m");
 
             Console.ReadKey();
         }
