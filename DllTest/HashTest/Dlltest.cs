04cb52f61a7cc24891062fe38997d9619c3a9cca
szajberpiraty <szajberpiraty@gmail.com>
2021-03-25 12:05:33 +0100

    Dlltest feladat

---
+++ DllTest/HashTest/Dlltest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using KRHash;
+
+namespace HashTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
