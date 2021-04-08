79fae6e546d9467a75988a6f2a8e7886caf9d0eb
szajberpiraty <szajberpiraty@gmail.com>
2021-04-08 08:46:48 +0200

    csudh

---
+++ csudh/csudh/Domain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csudh
+{
+    public class Domain
+    {
+        public string DomainName { get; set; }
+        public string IpAddress { get; set; }
+
+        public Domain(string sor,char hatarolo)
+        {
+            var e = sor.Split(hatarolo);
+            DomainName = e[0];
+            IpAddress = e[1];
+        }
+
+        public string DomainLevel(int level)
+        {
+            var levels = DomainName.Split('.');
+
+            //???
+        }
+    }
+}
---
+++ csudh/csudh/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csudh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Domain> domainek = new List<Domain>();
+            try
+            {
+                var sorok = File.ReadAllLines("csudh.txt",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    domainek.Add(new Domain(sorok[i],';'));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"3.feladat Domainek száma:{domainek.Count}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
