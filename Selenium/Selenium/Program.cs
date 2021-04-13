51f0fb6fd5c5576a68616904ab2bbd841432e85d
szajberpiraty <szajberpiraty@gmail.com>
2021-04-13 11:32:04 +0200

    Update Program.cs

--- Selenium/Selenium/Program.cs
+++ Selenium/Selenium/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -12,7 +13,16 @@ namespace Selenium
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://tanlaktanya.taszi.hu";
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.LinkText("Belépés")).Click();
+            driver.FindElement(By.Id("username")).SendKeys("seleniumtest");
 
+            Thread.Sleep(5000);
+
+            driver.Close();
 
             Console.ReadKey();
         }
