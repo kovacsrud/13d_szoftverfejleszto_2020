3353fcfba001b0d0d90a7381cffd7f0d242c83d4
szajberpiraty <szajberpiraty@gmail.com>
2021-04-19 09:58:03 +0200

    Teszt projekt létrehozás

---
+++ WpfAppiumTest/OsztasTest/TestOsztas.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace OsztasTest
+{
+    public class Tests
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
