9a187f6584faad884d6d8522dd8e16bc055cc40f
szajberpiraty <szajberpiraty@gmail.com>
2021-04-20 08:28:41 +0200

    Betöltés

--- ConsoleHianyzas/ConsoleHianyzas/Program.cs
+++ ConsoleHianyzas/ConsoleHianyzas/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace ConsoleHianyzas
     {
         static void Main(string[] args)
         {
+            List<Tanulo> tanulok = new List<Tanulo>();
+
+            try
+            {
+                var sorok = File.ReadAllLines("szeptember.csv",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    tanulok.Add(new Tanulo(sorok[i],';')); ;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"Adatok száma:{tanulok.Count}");
 
             Console.ReadKey();
         }
