6792065ea9a4afa6e61cebeba3871061bc68d0b9
szajberpiraty <szajberpiraty@gmail.com>
2021-04-22 10:15:11 +0200

    Fájl betöltés

--- Balkezesek/WpfBalkezesek/MainWindow.xaml.cs
+++ Balkezesek/WpfBalkezesek/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,29 @@ namespace WpfBalkezesek
     /// </summary>
     public partial class MainWindow : Window
     {
+        DoboData dobodata;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog()==true)
+            {
+                try
+                {
+                    dobodata = new DoboData(dialog.FileName,1);
+                    MessageBox.Show($"Adatok száma:{dobodata.Dobok.Count}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,"HIBA!");                    
+                }
+            }
+
         }
     }
 }
