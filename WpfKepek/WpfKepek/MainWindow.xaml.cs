c307b1c19a28225c5a03832569f4f1b26782043d
szajberpiraty <szajberpiraty@gmail.com>
2021-05-14 08:23:59 +0200

    Képek kezelése

---
+++ WpfKepek/WpfKepek/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKepek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            kep2.Source = new BitmapImage(new Uri(@"g:\tavtanitas\13dcsharp\000046.jpg"));
+            //kep2.Source = new BitmapImage(new Uri("000046.jpg"));
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog()==true)
+            {
+                kep2.Source = new BitmapImage(new Uri(dialog.FileName));
+            }
+
+        }
+    }
+}
