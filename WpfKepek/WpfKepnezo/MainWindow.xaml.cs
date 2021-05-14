d80934bcd6eb8a3bf0dc49c9554a6fd84f4bdf87
szajberpiraty <szajberpiraty@gmail.com>
2021-05-14 10:03:33 +0200

    Képnézegető

---
+++ WpfKepek/WpfKepnezo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKepnezo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "JPG fájlok (*.jpg)|*.jpg";
+            dialog.Multiselect = true;
+
+            if (dialog.ShowDialog()==true)
+            {
+                for (int i = 0; i < dialog.FileNames.Length; i++)
+                {
+                    var image = new Image();
+                    var bitmap = new BitmapImage(new Uri(dialog.FileNames[i]));
+                    image.Source = bitmap;
+                    image.Width = 200;
+                    wrappanelKepek.Children.Add(image);
+                }
+            }
+
+
+        }
+    }
+}
